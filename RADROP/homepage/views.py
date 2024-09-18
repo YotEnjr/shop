8b9ea27b4f732f8087f8d5dbc412c40e2002efb0
Никита Дмитриев <nikita.bmiyrieve@gmail.com>
2024-09-19 01:29:17 +0300

    add_templates_and_static_div

--- RADROP/homepage/views.py
+++ RADROP/homepage/views.py
@@ -1,13 +1,14 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # homepage/views.py
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Главная страница.
 
 
-def index(request):
-    return HttpResponse('Главная страница')
+#def index(request):
+#   return HttpResponse('Главная страница')
 
-# def index(request):
-#    return render(request, 'homepage/index.html', {})
+
+def index(request):
+    return render(request, 'homepage/index.html', {})
