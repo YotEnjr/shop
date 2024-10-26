26514d3db1f190f29b2f273ef817076f9aa8b2ca
YotEnjr <nikita.bmiyrieve@gmail.com>
2024-10-27 00:50:03 +0300

    admin-zone

--- Escape/userpage/admin.py
+++ Escape/userpage/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Userpage, UserBasket
 # Register your models here.
+admin.site.register(Userpage)
+admin.site.register(UserBasket) 
--- Escape/userpage/models.py
+++ Escape/userpage/models.py
@@ -3,12 +3,21 @@ from homepage.models import Products
 # Create your models here.
 
 class Userpage(models.Model):
-    login = models.CharField(max_length=128, unique=True)
-    password = models.CharField(max_length=32)
-    email = models.CharField(max_length=128, unique=True)
+    login = models.CharField(max_length=128, unique=True, verbose_name='типо логин')
+    password = models.CharField(max_length=32, verbose_name='пароль(надо шифрование придкмать)')
+    email = models.CharField(max_length=128, unique=True, verbose_name='почта бедолаги(тоже шифр нужен)')
 
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+    
+    def __str__(self):
+        return self.login 
 
-class UserBasket(Products):
-    product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='UserToBasket')
-    count = models.IntegerField()
+class UserBasket(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='UserToBasket', verbose_name='Продукт из каталога')
+    count = models.IntegerField(verbose_name='Кол-во шт.')
 
+    class Meta:
+        verbose_name = 'Карзина(заказ) Пользователя'
+        verbose_name_plural = 'Карзины(заказы) Пользователя'
